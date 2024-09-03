a13a5965510f770ed8289e8db4cc3b1bd3634633
ThornThavann <thavann.thorn@institute.pse.ngo>
2024-09-03 16:08:46 +0700

    header component

---
+++ src/Components/Background-img.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+function BackgroundSection({ backgroundImage, children, className }) {
+    return (
+        <div
+            className={`relative ${className}`}
+            style={{
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+               
+            }}
+        >
+            {children}
+        </div>
+    );
+}
+
+export default BackgroundSection;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import Navbar from '../../Components/Navbar';
+// import Navbar from '../../Components/Navbar';
+import Header from '../../Components/Header';
+
 
 function Home() {
     return(
        
         <div>
-        <Navbar></Navbar>
+             <Header></Header>
+       
+        {/* <Navbar></Navbar> */}
+       
         </div>
 
 
