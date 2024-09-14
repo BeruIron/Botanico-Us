fffc14f7e70e1ab1ae055424d73c86ebdfc8a0cb
khonsreyvorleak5796-4 <sreyvorleak.khon@institute.pse.ngo>
2024-09-14 11:32:32 +0700

    restaurent responsive

--- src/pages/ServicePage/Restaurent.jsx
+++ src/pages/ServicePage/Restaurent.jsx
@@ -2,98 +2,96 @@ import React from "react";
 import Navbar from "../../Components/Navbar";
 import Footer from "../../Components/Footer";
 import Button from "../../Components/Button";
-import { Link } from "react-router-dom";
-
+import img4 from "../../image/beer (37).jpg";
+import img2 from "../../image/beer (37).jpg";
+import img3 from "../../image/beer (37).jpg";
+import QuestionComponent from "../../Components/Question";
 function Restaurant() {
   return (
     <div>
-      <div className="h-[120px] bg-black ">
+      <div className="bg-black text-white ">
         <Navbar />
       </div>
-
-      <div className="h-[1870px] bg-[#D9D9D9]">
-        <div className="h-[100px] flex justify-center items-center">
-          <p className="text-4xl font-bold">Do you want to sell Botanico beer at your restaurant/bar?</p>
-        </div>
-        <div className="h-[100px]  flex justify-center items-center">
-          <p className="text-center text-2xl normal">
-            Are you interested in selling real Cambodian carft beer at your F&B
-            business? <br />
-            We provide draft beer as well as bottles. Contact us  <Link to="/Contact" className="text-red-600 text-xl normal">here.</Link>
-
-          </p>
-        </div>
-        <div className="h-[500px] bg-[#D9D9D9] flex justify-center items-center">
-          <div className="w-[243px] h-[328px]">
+      <div className="bg-gray-100 h-[550px] max-sm:h-[300px]">
+        <div className="h-[550px] max-sm:h-[300px]">
+          <QuestionComponent textquestion="Do you want to sell Botanico beer at your restaurant/bar?" />
+          <div className="flex justify-center overflow-hidden mt-[20px] ">
             <img
-              src="https://del.h-cdn.co/assets/cm/15/11/3200x3200/54f65d39ab05d_-_183341797.jpg"
-              alt=""
-              className="w-full h-full object-cover"
+              className="w-[250px] h-[330px] object-cover bg-orange-400 max-sm:w-[100px] max-sm:h-[150px] flex"
+              src={img2}
+              alt="Beer 1"
+            />
+            <img
+              className="w-[250px] h-[330px] object-cover bg-amber-900 mx-[10px] max-sm:w-[100px] max-sm:h-[150px] flex"
+              src={img3}
+              alt="Beer 2"
+            />
+            <img
+              className="w-[250px] h-[330px] object-cover bg-teal-900 max-sm:w-[100px] max-sm:h-[150px]   flex"
+              src={img4}
+              alt="Beer 3"
             />
           </div>
-          <div className="w-[243px] h-[328px] mx-[20px]">
-            <img
-              src="https://del.h-cdn.co/assets/cm/15/11/3200x3200/54f65d39ab05d_-_183341797.jpg"
-              alt=""
-              className="w-full h-full object-cover"
-            />
-          </div>
-          <div className="w-[243px] h-[328px] ">
-            <img
-              src="https://del.h-cdn.co/assets/cm/15/11/3200x3200/54f65d39ab05d_-_183341797.jpg"
-              alt=""
-              className="w-full h-full object-cover"
-            />
-          </div>
-        </div>
-
-        <div className="h-[1000px] bg-white flex justify-center items-center">
-          <div className="w-[84%] h-[768px] ">
-            <div className="flex">
-              <div className="w-[50%] h-[384px]">
-                <div className="flex justify-center h-[60px] items-center">
-                  <p className="text-2xl font-normal ">Draft</p>
-                  </div>
-                  <div className="h-[280px] mx-[30px]"> 
-              <p className="text-xl font-normal  leading-[55px]  whitespace-pre-wrap ">. Installation ,    maintenance   and   regular   cleaning  of  the  draft  machine.</p>
-              <p className="text-xl font-normal leading-[55px]  whitespace-pre-wrap ">. Free   delivery  of  kegs .</p>
-              <p className="text-xl font-normal  leading-[55px]   whitespace-pre-wrap">. On-sight  	training  for  bar  personal  and   service   staff  on  how  to  use  the  system 	and   about  the  product   itself .</p>
-              <p className="text-xl font-normal  leading-[55px]  whitespace-pre-wrap">. Provision 	of glassware   and   coasters .</p>
-              </div>
-              </div>
-              <div className="w-[50%] h-[384px] ">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtXYpxhwvLM3Uz8LvQRnj7NCTPsTZhREFetQ&s" alt=""
-                className="w-full h-full object-cover" />
-              </div>
-            </div>
-
-            <div className="flex">
-              <div className="w-[50%] h-[384px] ">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtXYpxhwvLM3Uz8LvQRnj7NCTPsTZhREFetQ&s" alt=""
-                className="w-full h-full object-cover" />
-              </div>
-              <div className="w-[50%] h-[384px] ">
-              <div className="flex justify-center h-[60px] items-center">
-                  <p className="text-2xl font-normal ">Draft</p>
-                  </div>
-                  <div className="h-[280px] mx-[30px]"> 
-                <p className="text-xl font-normal  leading-loose  whitespace-pre-wrap ">. Better  	solution  for  smaller   business   (little  space)  and / or less   expected 	consumption</p>
-                <p className="text-xl font-normal  leading-loose  whitespace-pre-wrap ">. Lower 	 MOQ (1 box  –  24   bottles ; mixed   cases  possible ) .</p>
-                <p  className="text-xl font-normal  leading-loose whitespace-pre-wrap ">. Free 	delivery</p>
-                <p className="text-xl font-normal  leading-loose  whitespace-pre-wrap ">. On-sight   	training   for bar  personal   and   service   staff  on  how  to  use  the  system 	 and   about  the  product   itself .</p>
-                <p className="text-xl font-normal  leading-loose  whitespace-pre-wrap ">. Provision  	of   glassware  and  coasters . </p>
-                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className=" flex items-center justify-center h-[150px] ">
-        <Button 
-          name="Contact Us"
-          color="Button-black"/>
-          
         </div>
       </div>
+
+      <div className="space-y-10 max-sm:w-[80%] justify-center mx-auto">
+        <div className=" bg-white flex justify-center space-x-10">
+          <ul className="list-disc pl-5 text-[20px] font-normal leading-[50px] max-sm:text-[10px] max-sm:leading-6 mt-[50px]">
+            <h2 className="font-bold"> Draft</h2>
+            <li>
+              Installation , maintenance and regular cleaning of the draft
+              machine.
+            </li>
+            <li>Free delivery of kegs .</li>
+            <li>
+              On-sight training for bar personal and service staff on how to use
+        <br />
+              the system and about the product itself .
+            </li>
+            <li>. Provision of glassware and coasters .</li>
+          </ul>
+
+          <div className="mt-[50px]">
+            <img 
+              src="https://lh3.googleusercontent.com/pw/AP1GczNU0E-c7VIs7_MhrHBAvC7B_ABXUO7t34ikO-BYVGeAPk2y5LFbFaonicicisY-0lud4zuVq5h18UZpxoIFccUJAG5YpNInAbluZWYBa6wT3Sco_Hcp2chgcFxPSIPg3nkSWnbRxgSxL7wShb0S29ix=w925-h617-s-no?authuser=0"
+              alt=""
+              className="w-[600px] max-sm:h-[200px] object-cover"
+            />
+          </div>
+        </div>
+
+        <div className=" bg-white flex justify-center space-x-10">
+          <div>
+            <img
+              src="https://lh3.googleusercontent.com/pw/AP1GczP_n2wnB21cLeySaMFAVnYhfhFqGNUlxhV2wcHvhT2cCxzlLf2K5KD-BF6YTpELITN7HeW40bvvBdv0IWpo0SbX4WkcfBKjY2pebwgv4CF-W-z9virnRfSWMAH1MZ5bgV6PZCb2IXlFFJK4Q4G-s24d=w925-h617-s-no?authuser=0"
+              alt=""
+              className="w-[600px] max-sm:h-[200px]  object-cover"
+            />
+          </div>
+
+          <ul className="list-disc pl-5 text-[20px] font-normal leading-[50px] max-sm:text-[10px] max-sm:leading-6">
+            <h2 className="text-[20px] font-bold  ">Draft</h2>
+            <li>
+              Better solution for smaller businesses (little space) and/or less <br />
+              
+              expected consumption
+            </li>
+            <li>Lower MOQ (1 box – 24 bottles; mixed cases possible)</li>
+            <li>Free delivery</li>
+            <li>
+              On-site training for bar personnel and service staff on how to use <br />
+            
+              the system and about the product itself
+            </li>
+            <li>Provision of glassware and coasters</li>
+          </ul>
+        </div>
+      </div>
+      <div className="bg-gray-100 mt-10 flex items-center justify-center h-[100px] ">
+        <Button name="Contact Us" color="Button-black" />
+      </div>
+
       <Footer />
     </div>
   );
