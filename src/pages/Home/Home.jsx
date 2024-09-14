9ab5ad8b1eb162c68eee1e90351a999b5ea47de2
khonsreyvorleak5796-4 <sreyvorleak.khon@institute.pse.ngo>
2024-09-14 11:27:45 +0700

    responsive home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,52 +11,60 @@ function Home() {
       <div  className="bg-black text-white">
       <Navbar />
       </div>
-       <div className="relative">
+      <div className="relative">
       <Bg />
-      <div className="absolute top-0 left-0 w-full">
-      </div>
-    </div>
-     <Title
+    </div>     
+      <Title
      titleText="BOTANICO"/>
 
-      {/* Content Section */}
-      <div className="relative z-10">
-        <div className="flex items-center w-full">
-          {/* Left Section */}
-          <div className="w-[45%] h-[700px]  p-8">
+
+      <div className=" w-[70%] h-[700px] mx-auto max-sm:w-[80%]">
+        <div className="flex justify-between  mt-16 max-sm:space-x-6">
+  
+          <div>
+
             <img
-              className="w-[561px] h-[606px] ml-[100px]"
-              src="https://i.pinimg.com/736x/3b/0a/e5/3b0ae56494b99e542a80e44f60b45960.jpg"
+              className=" w-[600px] h-[500px] object-cover max-sm:h-[350px] "
+              src="https://lh3.googleusercontent.com/pw/AP1GczMKbawwEISRFEdhIkfqhlIUi_RMZ6h2Ol0ts5K88GhQsyae9JYGafUWOooPOfeVBcjuaXkEwejeiH9h5JWZE2wavZP6vUSE7tO8JTBVEXokJLG__1_zwvxmuQMXBjKvTBLldo4xAFyNTrKuzTUiRpji=w1452-h968-s-no?authuser=0"
               alt="example"
             />
           </div>
-          {/* Right Section */}
-          <div className="w-[55%] h-[700px]  p-[25px]">
-            <h1 className="text-[48px] font-montserrat font-semibold ml-[150px]">
+
+          
+
+
+        
+          <div className="max-sm:w-[100%]">
+            <h1 className="text-[30px]  font-montserrat font-semibold max-sm:text-[15px]">
+
               Botanico Brewing Co.
-            </h1>
-            <p className="text-3xl my-8 ml-[80px] font-light">
-              In 2013, a few like-minded imbibers from the West joined <br /> our
-              brew-master in a common goal: to find a craft beer, <br />
-              microbrewery, or libation in Phnom Penh that was <br /> reminiscent
-              of home.
+            </h1> 
+
+            
+            <p className="text-[20px]  my-8 font-light max-sm:text-[10px] max-sm:my-2">
+              In 2013, a few like-minded imbibers from the West joined <br />
+              our brew-master in a common goal: to find a craft beer, <br />
+              microbrewery, or libation in Phnom Penh that was <br />
+              reminiscent of home.
             </p>
-            <p className="text-3xl ml-[80px] font-light">
+
+        
+            <p className="text-[20px]  font-light max-sm:text-[10px]">
               You, too, may have experienced a similar day with limited <br />
-              results. It was because of this that we took matters into our <br />
+              results. It was because of this that we took matters into our{" "}
+              <br />
               own hands and began crafting full-bodied beers with <br />
               iconic aromatics and a pleasing finish.
             </p>
-            <div  className="flex justify-center items-center mt-[50px]">
-            <Button
-            name="LearnMore"
-            color="Button-black"/>
-          </div>
+
+        
+            <div className="flex justify-center mt-[30px]">
+              <Button name="Learn More" color="Button-black" />
+            </div>
           </div>
         </div>
       </div>
 
-      {/* Footer */}
       <Footer />
     </div>
   );
