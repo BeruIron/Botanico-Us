b6353694920bfc989960c99a0ea2a245c449c187
Soychanleakhena <chanleakhena.soy@institute.pse.ngo>
2024-09-19 02:59:38 -0400

    change

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-
-import BeerFinder from "./pages/BeerFinder/BeerFinder";
-import Taproom from "./pages/TapRoom/TapRoom";
-import Restaurant from "./pages/ServicePage/Restaurant";
-import Event from "./pages/ServicePage/Event";
-import TestingBeer from "./pages/ServicePage/Testing";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home'; // Ensure this file exists
+import Story from './pages/StoryPage/Story'; // Ensure this file exists
+import BeerFinder from './pages/BeerFinder/BeerFinder';
+import Taproom from './pages/TapRoom/TapRoom';
+import Restaurant from './pages/ServicePage/Restaurant';
+import Event from './pages/ServicePage/Event';
+import TestingBeer from './pages/ServicePage/Testing';
 import Blog from './pages/Blog/Blog';
-import OurBeer from "./pages/OurBeer/OurBeer";
-import Detail from "./pages/OurBeer/Detail";
+import OurBeer from './pages/OurBeer/OurBeer';
+import Detail from './pages/OurBeer/Detail';
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-
         <Route path="/story" element={<Story />} />
         <Route path="/ourBeer" element={<OurBeer />} />
         <Route path="/ourBeer/detail" element={<Detail />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/beerFinder" element={<BeerFinder />} />
         <Route path="/restaurant" element={<Restaurant />} />
-        <Route path="/story" element={<Story />} />
         <Route path="/event" element={<Event />} />
         <Route path="/testing" element={<TestingBeer />} />
         <Route path="/taproom" element={<Taproom />} />
-          <Route path="/blog" element={<Blog/>} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </>
   );
-
 };
 
 export default App;
