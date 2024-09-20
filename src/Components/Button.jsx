d84c408d22e177a1435c9cce3218222bb990b843
Soychanleakhena <chanleakhena.soy@institute.pse.ngo>
2024-09-20 04:17:50 -0400

    feature/contact-page

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Button({ name, color, onClick }) {
+function Button({ name, color, onClick,className }) {
   return (
     <button
-      className={`w-[120px] h-[40px] text-white bg-slate-400 my-1  hover:bg-gray-600 max-sm:w-[70px] max-sm:h-[25px] max-sm:text-[10px] ${color}`}
+      className={`w-[100px] h-[50px] py-3 text-white bg-slate-400 my-1  hover:bg-gray-600 max-sm:w-[70px] max-sm:h-[25px] max-sm:text-[12px] ${color} ${className}`}
       onClick={onClick}
     >
       {name}
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Navbar from "../../Components/Navbar";
 import Button from "../../Components/Button";
-
 import {
   FaFacebookF,
   FaInstagram,
@@ -29,17 +28,29 @@ const ContactForm = () => {
       ...prevData,
       [name]: value,
     }));
+
+    // Clear the error when the user starts typing
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: "", // Clear the specific error
+    }));
   };
 
   const validateForm = () => {
     const newErrors = {};
     if (!formData.firstName) newErrors.firstName = "First name is required.";
     if (!formData.lastName) newErrors.lastName = "Last name is required.";
-    if (!formData.email) newErrors.email = "Email is required.";
-    else if (!/\S+@\S+\.\S+/.test(formData.email))
+    if (!formData.email) {
+      newErrors.email = "Email is required.";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
       newErrors.email = "Email is invalid.";
+    }
     if (!formData.message) newErrors.message = "Message is required.";
-    if (!formData.phone) newErrors.phone = "phone number is required.";
+    if (!formData.phone) {
+      newErrors.phone = "Phone number is required.";
+    } else if (!/^[0-9+]{7,10}$/.test(formData.phone)) {
+      newErrors.phone = "Phone number is invalid. It should be 7 to 10 digits.";
+    }
 
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
@@ -49,7 +60,6 @@ const ContactForm = () => {
     event.preventDefault();
 
     if (!validateForm()) return;
-
     setResult("Sending....");
     const form = event.target;
     const formData = new FormData(form);
@@ -81,182 +91,84 @@ const ContactForm = () => {
     }
   };
 
+
   return (
     <>
       <Navbar />
 
-      <div className="w-[50%] mx-auto my-10 p-6 rounded-lg ">
-        <div >
-          <h1 className="text-3xl font-bold text-blue-900 text-center mt-[20px] font-montserrat max-sm:text-2xl ">
-            CONTACT US
-          </h1>
-        </div>
+      <div className="w-[50%] mx-auto my-10 p-6 rounded-lg max-sm:w-[90%]">
+        <h1 className="text-3xl font-bold text-blue-600 text-center mt-[20px]">CONTACT US</h1>
 
-        <form
-          onSubmit={handleSubmit}
-          className="space-y-8 mt-[50px] text-xl font-montserrat  "
-        >
+        <form onSubmit={handleSubmit} className="space-y-8 mt-[50px] text-xl font-montserrat">
           <div>
-            <label className="block text-gray-700 font-bold ">First name</label>
+            <label className="block text-gray-700 font-bold text-[18px] max-sm:text-[15px]">First name</label>
             <input
               type="text"
               name="firstName"
               value={formData.firstName}
               onChange={handleChange}
               placeholder="Enter your first name"
-              className="w-full px-4 py-3 border border-gray-300 rounded-md max-sm:w-[300px] max-sm:mx-auto"
+              className={`w-full px-4 py-3 border  ${errors.firstName ? 'border-red-500' : 'border-gray-300'} placeholder:text-[18px] placeholder:max-sm:text-[15px] max-sm:h-[50px]`}
             />
-            {errors.firstName && (
-              <p className="text-red-500">{errors.firstName}</p>
-            )}
+            {errors.firstName && <p className="text-red-500 text-[17px]">{errors.firstName}</p>}
           </div>
           <div>
-            <label className="block text-gray-700 font-bold">Last name</label>
+            <label className="block text-gray-700 font-bold text-[18px] max-sm:text-[15px]">Last name</label>
             <input
               type="text"
               name="lastName"
               value={formData.lastName}
               onChange={handleChange}
               placeholder="Enter your last name"
-              className="w-full px-4 py-3 border border-gray-300 rounded-md"
+              className={`w-full px-4 py-3 border  ${errors.lastName ? 'border-red-500' : 'border-gray-300'} placeholder:text-[18px] placeholder:max-sm:text-[15px] max-sm:h-[50px]`}
             />
-            {errors.lastName && (
-              <p className="text-red-500">{errors.lastName}</p>
-            )}
+            {errors.lastName && <p className="text-red-500 text-[17px]">{errors.lastName}</p>}
           </div>
           <div>
-            <label className="block text-gray-700 font-bold">Email</label>
+            <label className="block text-gray-700 font-bold text-[18px] max-sm:text-[15px]">Email</label>
             <input
               type="email"
               name="email"
               value={formData.email}
               onChange={handleChange}
               placeholder="Enter your email"
-              className="w-full px-4 py-3 border border-gray-300 rounded-md"
+              className={`w-full px-4 py-3 border  ${errors.email ? 'border-red-500' : 'border-gray-300'} placeholder:text-[18px] placeholder:max-sm:text-[15px] max-sm:h-[50px]`}
             />
-            {errors.email && <p className="text-red-500">{errors.email}</p>}
+            {errors.email && <p className="text-red-500 text-[17px]">{errors.email}</p>}
           </div>
           <div>
-            <label className="block text-gray-700 font-bold">
-              Phone number
-            </label>
+            <label className="block text-gray-700 font-bold text-[18px] max-sm:text-[15px]">Phone number</label>
             <input
               type="tel"
               name="phone"
               value={formData.phone}
               onChange={handleChange}
               placeholder="Enter your phone number"
-              className="w-full px-4 py-3 border border-gray-300 rounded-md"
+              className={`w-full px-4 py-3 border  ${errors.phone ? 'border-red-500' : 'border-gray-300'} placeholder:text-[18px] placeholder:max-sm:text-[15px] max-sm:h-[50px]`}
             />
-            {errors.phone && <p className="text-red-500">{errors.phone}</p>}
+            {errors.phone && <p className="text-red-500 text-[17px]">{errors.phone}</p>}
           </div>
           <div>
-            <label className="block text-gray-700 font-bold">Message</label>
+            <label className="block text-gray-700 font-bold text-[18px] max-sm:text-[15px]">Message</label>
             <textarea
               name="message"
               value={formData.message}
               onChange={handleChange}
               placeholder="Type your message here..."
-              className="w-full px-4 py-3 border border-gray-300 rounded-md"
+              className={`w-full px-4 py-3 border  ${errors.message ? 'border-red-500' : 'border-gray-300'} placeholder:text-[18px] placeholder:max-sm:text-[15px]`}
               rows="5"
             />
-            {errors.message && <p className="text-red-500">{errors.message}</p>}
+            {errors.message && <p className="text-red-500 text-[17px]">{errors.message}</p>}
           </div>
           <div className="flex justify-center items-center mt-4 pb-8">
-            <Button type="submit" name="Submit" color="Button-black" />
+            <Button type="submit" name="Submit" color="Button-black" className="max-sm:w-[25%] max-sm:h-[40px] max-sm:py-2" />
           </div>
         </form>
-        <div className="w-full">
-          <div className="flex justify-between py-8">
-            <div className="flex-1 flex justify-center">
-              <div>
-                <h2 className="text-2xl font-bold">Social media channels</h2>
-                <hr className="w-[100px] border-black border-1 my-1" />
-                <div className="mt-[40px]">
-                  <ul className="space-y-4">
-                    <li>
-                      <a
-                        href="https://m.facebook.com/botanicobrewingco?mibextid=LQQJ4d"
-                        className="flex items-center space-x-4 hover:text-gray-400"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <FaFacebookF className="text-blue-600 text-3xl" />
-                        <span className="text-lg">Facebook</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="https://www.instagram.com/yourInstagramLink"
-                        className="flex items-center space-x-4 hover:text-gray-400"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <FaInstagram className="text-pink-500 text-3xl" />
-                        <span className="text-lg">Instagram</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="https://www.tiktok.com/@yourTiktokUsername"
-                        className="flex items-center space-x-4 hover:text-gray-400"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <FaTiktok className="text-black text-3xl" />
-                        <span className="text-lg">TikTok</span>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+        
+         </div>
 
-            {/* Contact Section */}
-            <div className="flex-1 flex justify-center items-center">
-              <div>
-                <h2 className="text-2xl font-bold">Contact</h2>
-                <hr className="w-[50px] border-black border-1 my-1" />
-                <div className="mt-[40px]">
-                  <ul className="space-y-4">
-                    <li className="flex items-center space-x-4 hover:text-gray-400">
-                      <a
-                        href="mailto:chanleakhena.soy@institute.pse.ngo"
-                        className="flex items-center space-x-4 hover:text-gray-400"
-                      >
-                        <FaEnvelope className="text-gray-600 text-3xl" />
-                        <span className="text-lg">Email</span>
-                      </a>
-                    </li>
-                    <li className="flex items-center space-x-4 hover:text-gray-400">
-                      <a
-                        href="https://t.me/yourTelegramUsername"
-                        className="flex items-center space-x-4 hover:text-gray-400"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <FaTelegramPlane className="text-blue-500 text-3xl" />
-                        <span className="text-lg">Telegram</span>
-                      </a>
-                    </li>
-                    <li className="flex items-center space-x-4 hover:text-gray-400">
-                      <a
-                        href="tel:+88570513499"
-                        className="flex items-center space-x-4 hover:text-gray-400"
-                      >
-                        <FaPhone className="text-blue-600 text-3xl" />
-                        <span className="text-lg">+885 70513499</span>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <Footer />
+        <Footer />
+     
     </>
   );
 };
