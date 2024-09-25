2f4f07d5c2cf7083565318d244f33596bc6e616e
khonsreyvorleak5796-4 <sreyvorleak.khon@institute.pse.ngo>
2024-09-25 15:00:44 +0700

    translate

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,99 +1,70 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
-import i18n from 'i18next';
-
 import Image from "../image/logomain.png";
-
-
-// import { FaLanguage } from "react-icons/fa";
 import { RiMenu3Fill, RiArrowDownSFill } from "react-icons/ri";
 import { changeLanguage } from "i18next";
-
+import { useTranslation } from 'react-i18next';
 function Navbar() {
+  const { t } = useTranslation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+
   const content = (
     <ul className="flex gap-6 text-center text-white ml-[100px]">
       <li>
-        <Link to="/" className="hover:text-gray-400">
-          HOME
-        </Link>
+        <Link to="/" className="hover:text-gray-400"> {t("Navnar1")} </Link>
       </li>
       <li>
-        <Link to="/Story" className="hover:text-gray-400">
-          STORY
-        </Link>
+        <Link to="/Story" className="hover:text-gray-400">{t("Navnar2")}</Link>
       </li>
       <li>
-              <Link to="/ourbeer" className="hover:text-gray-400">
-                OUR BEER
-              </Link>
-            </li>
+        <Link to="/ourbeer" className="hover:text-gray-400">{t("Navnar3")}</Link>
+      </li>
       <li className="relative group">
         <Link to="/service" className="flex items-center hover:text-gray-400 focus:outline-none">
-          <span>SERVICE</span>
+          <span>{t("Navnar4")}</span>
           <RiArrowDownSFill className="ml-2" />
         </Link>
-
         <ul className="absolute left-0 mt-2 w-48 bg-gray-700 text-white rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20">
           <li>
-            <Link to="/event" className="block px-4 py-2 hover:bg-gray-600">
-              Event
-            </Link>
+            <Link to="/event" className="block px-4 py-2 hover:bg-gray-600">{t("Navnar5")}</Link>
           </li>
           <li>
-            <Link to="/testing" className="block px-4 py-2 hover:bg-gray-600">
-              Testing Beer
-            </Link>
+            <Link to="/tasting" className="block px-4 py-2 hover:bg-gray-600">{t("Navnar6")}</Link>
           </li>
           <li>
-          <Link
-                    to="/restaurant"
-                    className="block px-4 py-2 hover:bg-gray-500"
-                  >
-                    Restaurant
-                  </Link>
+            <Link to="/restaurant" className="block px-4 py-2 hover:bg-gray-500">{t("Navnar7")}</Link>
           </li>
         </ul>
       </li>
       <li>
-        <Link to="/BeerFinder" className="hover:text-gray-400">
-          BEER FINDER
-        </Link>
+        <Link to="/BeerFinder" className="hover:text-gray-400">{t("Navnar8")} </Link>
       </li>
       <li>
-        <Link to="/taproom" className="hover:text-gray-400">
-          TAP ROOM
-        </Link>
+        <Link to="/taproom" className="hover:text-gray-400">{t("Navnar9")}</Link>
       </li>
       <li>
-        <Link to="/contact" className="hover:text-gray-400">
-          CONTACT
-        </Link>
+        <Link to="/contact" className="hover:text-gray-400">{t("Navnar10")}</Link>
       </li>
       <li>
-        <Link to="/blog" className="hover:text-gray-400">
-          BLOG
-        </Link>
+        <Link to="/blog" className="hover:text-gray-400">{t("Navnar11")}</Link>
       </li>
     </ul>
   );
 
   return (
-    <nav className="relative  bg-gray-800">
-      <div className=" flex justify-between items-center px-4 py-4">
+    <nav className="relative bg-black">
+      <div className="flex justify-between items-center px-4 py-4">
         <div className="overflow-hidden">
           <Link to="/">
             <img
               src={Image}
               alt="BOTANICO Logo"
-
-              className=" h-[80px] w-[180px] max-sm:h-[60px] max-sm:w-[130px] "
+              className="h-[80px] w-[180px] max-sm:h-[60px] max-sm:w-[130px]"
             />
           </Link>
         </div>
@@ -107,91 +78,65 @@ function Navbar() {
           />
         </div>
 
-        <div className="flex p-2 bg-slate-300 gap-3">
-        {/* <FaLanguage className="hidden md:block text-white text-5xl mr-[50px]" /> */}
-        <Link onClick={() => changeLanguage('en')} >ENG</Link>
-        <div> | </div>
-        <Link onClick={() => changeLanguage('kh')} >KH</Link>
-        </div>
 
-      </div>
-{isMenuOpen && (
-        <div className="md:hidden bg-gray-700 text-white">
-          <ul className="flex flex-col items-center gap-6 py-4">
-            <li>
-              <Link to="/" className="hover:text-gray-400">
-                HOME
-              </Link>
-            </li>
-            <li>
-              <Link to="/Story" className="hover:text-gray-400">
-                STORY
-              </Link>
-            </li>
-            <li>
-              <Link to="/ourbeer" className="hover:text-gray-400">
-                OUR BEER
-              </Link>
-            </li>
-            <li className="relative">
-              <Link to="/service" className="flex items-center hover:text-gray-400 focus:outline-none">
-                <span>SERVICE</span>
-                <RiArrowDownSFill className="ml-2" />
-              </Link>
-              <ul className="bg-gray-600 mt-2">
-                <li>
-                  <Link
-                    to="/event"
-                    className="block px-4 py-2 hover:bg-gray-500"
-                  >
-                    Event
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/testing"
-                    className="block px-4 py-2 hover:bg-gray-500"
-                  >
-                    Testing Beer
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/restaurant"
-                    className="block px-4 py-2 hover:bg-gray-500"
-                  >
-                    Restaurant
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link to="/BeerFinder" className="hover:text-gray-400">
-                BEER FINDER
-              </Link>
-            </li>
-            <li>
-              <Link to="/taproom" className="hover:text-gray-400">
-                TAP ROOM
-              </Link>
-            </li>
-            <li>
-              <Link to="/contact" className="hover:text-gray-400">
-                CONTACT
-              </Link>
-            </li>
-            <li>
-              <Link to="/blog" className="hover:text-gray-400">
-                BLOG
-              </Link>
-            </li>
-          </ul>
+        <div className="hidden md:flex p-2 bg-blue-800 gap-3 text-white font-bold">
+          <Link onClick={() => changeLanguage('en')}>ENG</Link>
+          <div> | </div>
+          <Link onClick={() => changeLanguage('kh')}>KH</Link>
         </div>
-      )}
+      </div>
+      {isMenuOpen && (
+  <div className="md:hidden bg-gray-700 text-white">
+    <ul className="flex flex-col items-center gap-6 py-4">
+    <li className="flex gap-2 bg-blue-800 p-2 text-white font-bold">
+        <Link onClick={() => changeLanguage('en')} className="hover:text-gray-400">ENG</Link>
+        <span>|</span>
+        <Link onClick={() => changeLanguage('kh')} className="hover:text-gray-400">KH</Link>
+      </li>
+      <li>
+        <Link to="/" className="hover:text-gray-400"> {t("Navnar1")} </Link>
+      </li>
+      <li>
+        <Link to="/Story" className="hover:text-gray-400"> {t("Navnar2")} </Link>
+      </li>
+      <li>
+        <Link to="/ourbeer" className="hover:text-gray-400"> {t("Navnar3")}</Link>
+      </li>
+      <li className="relative">
+        <Link to="/service" className="flex items-center hover:text-gray-400 focus:outline-none">
+          <span>{t("Navnar4")}</span>
+          <RiArrowDownSFill className="ml-2" />
+        </Link>
+        <ul className="bg-gray-600 mt-2">
+          <li>
+            <Link to="/event" className="block px-4 py-2 hover:bg-gray-500"> {t("Navnar5")} </Link>
+          </li>
+          <li>
+            <Link to="/tasting" className="block px-4 py-2 hover:bg-gray-500"> {t("Navnar6")}</Link>
+          </li>
+          <li>
+            <Link to="/restaurant" className="block px-4 py-2 hover:bg-gray-500"> {t("Navnar7")} </Link>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <Link to="/BeerFinder" className="hover:text-gray-400"> {t("Navnar8")}</Link>
+      </li>
+      <li>
+        <Link to="/taproom" className="hover:text-gray-400"> {t("Navnar9")}</Link>
+      </li>
+      <li>
+        <Link to="/contact" className="hover:text-gray-400"> {t("Navnar10")} </Link>
+      </li>
+      <li>
+        <Link to="/blog" className="hover:text-gray-400"> {t("Navnar11")} </Link>
+      </li>
+    </ul>
+  </div>
+)}
+
     </nav>
   );
 }
 
-
 export default Navbar;
-
--- src/pages/StoryPage/Story.jsx
+++ src/pages/StoryPage/Story.jsx
@@ -4,10 +4,18 @@ import Sa from "../../Components/SlideAutomatic";
 import Button from "../../Components/Button";
 import Footer from "../../Components/Footer";
 import Title from "../../Components/Tittle";
-import { useTranslation } from "react-i18next";
 
-import image from "../../image/story1.png";
-import image1 from "../../image/story.png";
+import image from '../../image/story.png'
+import image1 from '../../image/DSC01542.jpg'
+
+
+import { useTranslation } from 'react-i18next';
+
+
+import { Link } from 'react-router-dom';
+
+
+
 
 function Story() {
   const { t } = useTranslation();
@@ -21,63 +29,77 @@ function Story() {
 
       <Title titleText={t("wearebotanico")}></Title>
       <div className=" w-[70%] max-sm:w-[90%] mx-auto">
-        <p className="text-[20px]  font-abc leading-10 h-[130px] max-sm:leading-7 items-center max-sm:text-[10px] max-sm:h-[150px] flex">
-          {t("wearebotanico-p1")}
-          {t("wearebotanico-p2")}
-          {t("wearebotanico-p3")}
+
+        <p className="text-[20px]  font-light leading-10 h-[130px] max-sm:leading-7 items-center max-sm:text-[12px] max-sm:h-[150px] flex">
+          {t("wearebotanico-p1")} 
+          {t("wearebotanico-p2")} 
+          {t("wearebotanico-p3")} 
+
         </p>
       </div>
       <Title titleText={t("ourstory")}></Title>
       <div className="w-[70%] max-sm:w-[90%] mx-auto">
         <div className="flex max-sm:flex-col flex-row mt-[50px]  mx-auto max-sm:space-x-0 space-x-10">
           <img
-            src={image}
+            src={image1}
             alt="Brewery"
             className="w-[500px] h-[400px] object-cover"
           />
           <div>
-            <p className="text-[20px] font-abc leading-10 h-[170px] max-sm:leading-7 max-sm:text-[10px] max-sm:h-[170px] flex">
-              {t("ourstory-p")}
-              {t("ourstory-p1")}
-              {t("ourstory-p2")}
-              {t("ourstory-p3")}
-              {t("ourstory-p4")}
-              {t("ourstory-p5")}
+
+
+            <p className="text-[20px] font-light leading-10 h-[170px] max-sm:leading-7 max-sm:text-[12px] max-sm:h-[170px] flex">
+             {t("ourstory-p")}
+             {t("ourstory-p1")}
+             {t("ourstory-p2")}
+             {t("ourstory-p3")}
+             {t("ourstory-p4")}
+             {t("ourstory-p5")}
+
             </p>
-            <p className="text-[20px]  font-abc leading-10 max-sm:leading-7 max-sm:h-[150px] max-sm:text-[10px] h-[100px]">
-              {t("ourstory-h")}
-              {t("ourstory-h1")}
-              {t("ourstory-h2")}
-              {t("ourstory-h3")}
+            <p className="text-[20px] pt-16 font-light leading-10 max-sm:leading-7 max-sm:h-[150px] max-sm:text-[12px] h-[100px]">
+            {t("ourstory-h")}
+            {t("ourstory-h1")}
+            {t("ourstory-h2")}
+            {t("ourstory-h3")}
+
             </p>
           </div>
         </div>
         <div>
-          <p className="text-[20px] leading-10 max-sm:leading-7 font-abc max-sm:text-[10px] items-center flex h-[200px]">
-            {t("ourstory-a")}
-            {t("ourstory-a1")}
-            {t("ourstory-a2")}
-            {t("ourstory-a3")}
-            {t("ourstory-a4")}
-            {t("ourstory-a5")}
-          </p>
 
-          <p className="font-abc font-bold  text-[30px] max-sm:mt-[50px] max-sm:text-[20px]">
-            {" "}
-           {t("ourstory-t")}
-          </p>
+           <p className="text-[20px] leading-10 max-sm:leading-7 font-light max-sm:text-[12px] items-center flex h-[200px]">
+           {t("ourstory-a")}
+             {t("ourstory-a1")}
+             {t("ourstory-a2")}
+             {t("ourstory-a3")}
+             {t("ourstory-a4")}
+             {t("ourstory-a5")}
+
+        </p>
+
+        <p className="font-abc font-bold  text-[30px] max-sm:mt-[50px] max-sm:text-[20px]"> Real Cambodian Craft Beer</p>
+
         </div>
 
+
+
+       
         <div className=" mt-[50px] ">
           <img
-            src={image1}
+            src={image}
             alt="Brewery"
             className=" w-full h-[700px] max-sm:h-[400px] object-cover"
           />
         </div>
 
         <div className="flex justify-center items-center font-abc h-[150px] max-sm:h-[50px]">
-          <Button name={t("ourstory-butt")} className="Button-black" />
+
+        <Link to="/ourBeer">
+
+          <Button name="View Products" color="Button-black" />
+          </Link>
+
         </div>
       </div>
 
